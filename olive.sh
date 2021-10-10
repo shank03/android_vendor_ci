947b36001aa057d7b6a2433fe57c38a60fb9cfff
deadlylxrd <IAmDeadlylxrd@gmail.com>
2021-10-10 21:57:10 +0500

    Create olive.sh

---
+++ olive.sh
@@ -0,0 +1,5 @@
+# Havoc-OS
+#
+# Track our display hals
+rm -rf hardware/qcom-caf/msm8996/display
+git clone https://github.com/mi-sdm439/android_hardware_qcom_display -b lineage-18.1-caf-msm8996 hardware/qcom-caf/msm8996/display
