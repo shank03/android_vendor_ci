7f6d19514c3f1a97b677b25908cd9a9a0a7dfa3e
Aryan Karan <aryankaran28022004@gmail.com>
2021-10-17 16:05:19 +0530

    Create onclite.sh

---
+++ onclite.sh
@@ -0,0 +1,7 @@
+# Build Type
+
+export buildtype="user"
+
+# Trees
+git clone https://github.com/Havoc-Devices/kernel_xiaomi_onclite kernel/xiaomi/onclite
+git clone https://github.com/Havoc-Devices/vendor_xiaomi_onclite vendor/xiaomi/onclite
