1cedbe33ac2bb6fdd3997e2cea17e89cef8ba127
Aryan Karan <aryankaran28022004@gmail.com>
2021-10-19 07:14:39 +0530

    No more needed to clone trees

--- onclite.sh
+++ onclite.sh
@@ -1,7 +1,3 @@
 # Build Type
 
 export buildtype="user"
-
-# Trees
-git clone https://github.com/Havoc-Devices/kernel_xiaomi_onclite kernel/xiaomi/onclite
-git clone https://github.com/Havoc-Devices/vendor_xiaomi_onclite vendor/xiaomi/onclite
