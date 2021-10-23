9a97b038844070cd6306c11ee9bda313018d2a6a
adnan-44 <mohd.adnan0405@gmail.com>
2021-10-24 01:14:01 +0530

    vince: Add vince
    * Remove source synced hals.
    * Set build type as "user"
    
    Signed-off-by: adnan-44 <mohd.adnan0405@gmail.com>

---
+++ vince.sh
@@ -0,0 +1,9 @@
+# Havoc-OS
+
+# Build Type, To build "user" build
+export buildtype="user"
+
+# For Hals
+rm -rf hardware/qcom-caf/msm8996/display hardware/qcom-caf/msm8996/audio hardware/qcom-caf/msm8996/media
+
+# All cloned stuff should be hosted on havoc-devices , otherwise it won't be cloned
