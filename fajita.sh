69fbb4c518d166afa58e4f537bd029a258bffd29
optimus9811 <vikasyadav5073@gmail.com>
2021-10-23 14:49:35 +0200

    Add oneplus 6|6T hals
    Signed-off-by: optimus9811 <vikasyadav5073@gmail.com>

---
+++ fajita.sh
@@ -0,0 +1,11 @@
+# Havoc-OS
+
+# Hals 
+rm -rf hardware/qcom-caf/sdm845/display
+git clone https://github.com/Havoc-Devices/android_sdm845_qcom_display  -b sdm845   hardware/qcom-caf/sdm845/display
+
+rm -rf hardware/qcom-caf/sdm845/media
+https://github.com/Havoc-Devices/android_sdm845_qcom_media  -b sdm845  hardware/qcom-caf/sdm845/media
+
+rm -rf hardware/qcom-caf/sdm845/audio
+https://github.com/Havoc-Devices/android_sdm845_qcom_audio   -b sdm845  hardware/qcom-caf/sdm845/audio
