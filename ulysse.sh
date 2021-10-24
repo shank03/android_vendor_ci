d7c3472ae37082b61b3561fd3d2d6034e9567add
Heinz Der Flugel <heinzdf@gabuterscape.my.id>
2021-10-24 18:37:47 +0700

    ulysse: remove custom hals

--- ulysse.sh
+++ ulysse.sh
@@ -8,9 +8,3 @@ git clone https://github.com/Gabuters-Dev/gabuters-clang $gclang
 else
 echo "Gabuters clang exist, skipping.."
 fi
-
-rm -rf hardware/qcom-caf/msm8996/media hardware/qcom-caf/msm8996/audio hardware/qcom-caf/msm8996/display
-git clone https://github.com/Jabiyeff-Project/android_hardware_qcom_display -b 11.0 hardware/qcom-caf/msm8996/display
-git clone https://github.com/Jabiyeff-Project/android_hardware_qcom_audio -b 11.0 hardware/qcom-caf/msm8996/audio
-git clone https://github.com/Jabiyeff-Project/android_hardware_qcom_media -b 11.0 hardware/qcom-caf/msm8996/media
-git clone https://github.com/Gabuters-Dev/vendor_xiaomi_dirac -b part vendor/xiaomi/part
