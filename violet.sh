a3838c05ab94c88efc422f90758528430fcf7558
Logan <athuldineshan5@gmail.com>
2021-10-24 19:49:44 +0530

    violet: Hal script
    Signed-off-by: Logan <athuldineshan5@gmail.com>

---
+++ violet.sh
@@ -0,0 +1,7 @@
+
+# Havoc-OS
+
+# Hals
+rm -rf hardware/qcom-caf/sm8150/display
+
+git clone https://github.com/Havoc-Devices/android_hardware_qcom-caf_sm8150_display.git -b eleven hardware/qcom-caf/sm8150/display
