ab061587e615f18cd6dce39c9e7dce411ce411ef
chandu078 <chandudyavanapelli03@gmail.com>
2021-10-24 12:11:49 +0000

    kebab: Clone OnePlus Camera
    Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>

--- kebab.sh
+++ kebab.sh
@@ -1,2 +1,3 @@
 bash device/oneplus/sm8250-common/patch/patch.sh
+git clone -b eleven https://gitlab.com/havoc-devices/vendor_oneplus_addons_camera.git vendor/oneplus/addons/camera
 export buildtype="user"
