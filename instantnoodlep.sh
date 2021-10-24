59d4b166732f46b58ae14eed93cb7d21b4d03199
Chris Chen <70048372+cjh1249131356@users.noreply.github.com>
2021-10-24 20:06:55 +0800

    instantnoodlep: Clone opcamera

--- instantnoodlep.sh
+++ instantnoodlep.sh
@@ -1,2 +1,3 @@
 bash device/oneplus/sm8250-common/patch/patch.sh
+git clone -b eleven https://gitlab.com/havoc-devices/android_vendor_oneplus_opcamera vendor/oneplus/opcamera
 export buildtype="user"
