bfc3822614372586cf16ccce23ec58c4069fa7ec
Tushar Mahajan <mahajant99@gmail.com>
2021-10-26 00:14:08 +0530

    surya: Clone clang
    Signed-off-by: Tushar Mahajan <mahajant99@gmail.com>

---
+++ surya.sh
@@ -0,0 +1,6 @@
+# Havoc-OS
+
+# All cloned stuff should be hosted on havoc-devices , otherwise it won't be cloned
+
+rm -rf prebuilts/clang/host/linux-x86/clang-r433403
+git clone https://gitlab.com/havoc-devices/android_prebuilts_clang_host_linux-x86_clang-r433403.git prebuilts/clang/host/linux-x86/clang-r433403
