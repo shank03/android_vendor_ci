542e4d4dad787c2c091bb8fbb6b7a5046b45f5a4
optimus9811 <vikasyadav5073@gmail.com>
2021-10-25 17:06:31 +0200

    Clone clang & update hals repo link

--- enchilada.sh
+++ enchilada.sh
@@ -1,11 +1,14 @@
 # Havoc-OS
 
-# Hals 
+#hals
 rm -rf hardware/qcom-caf/sdm845/display
 git clone https://github.com/Havoc-Devices/android_sdm845_qcom_display  -b sdm845   hardware/qcom-caf/sdm845/display
 
-rm -rf hardware/qcom-caf/sdm845/media
-https://github.com/Havoc-Devices/android_sdm845_qcom_media  -b sdm845  hardware/qcom-caf/sdm845/media
-
 rm -rf hardware/qcom-caf/sdm845/audio
-https://github.com/Havoc-Devices/android_sdm845_qcom_audio   -b sdm845  hardware/qcom-caf/sdm845/audio
+git clone https://github.com/Havoc-Devices/android_sdm845_qcom_audio   -b sdm845  hardware/qcom-caf/sdm845/audio
+
+rm -rf hardware/qcom-caf/sdm845/media
+git clone https://github.com/Havoc-Devices/android_sdm845_qcom_media  -b sdm845  hardware/qcom-caf/sdm845/media
+
+#clang
+git clone -b eleven https://github.com/Havoc-Devices/android_prebuilts_clang_host_linux-x86_clang-r433403 prebuilts/clang/host/linux-x86/clang-r433403
--- fajita.sh
+++ fajita.sh
@@ -10,3 +10,5 @@ git clone https://github.com/Havoc-Devices/android_sdm845_qcom_audio   -b sdm845
 rm -rf hardware/qcom-caf/sdm845/media
 git clone https://github.com/Havoc-Devices/android_sdm845_qcom_media  -b sdm845  hardware/qcom-caf/sdm845/media
 
+#clang
+git clone -b eleven https://github.com/Havoc-Devices/android_prebuilts_clang_host_linux-x86_clang-r433403 prebuilts/clang/host/linux-x86/clang-r433403
