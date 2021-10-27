c54892bd38b33b56e85ae25fb2c8909e8c4ec8a3
adnan-44 <mohd.adnan0405@gmail.com>
2021-10-27 13:56:32 +0530

    vince: Clone hals from here
    * Display, media, audio and wlan
    * Removed wlan hal directory also
    
    Signed-off-by: adnan-44 <mohd.adnan0405@gmail.com>

--- vince.sh
+++ vince.sh
@@ -4,6 +4,10 @@
 export buildtype="user"
 
 # For Hals
-rm -rf hardware/qcom-caf/msm8996/display hardware/qcom-caf/msm8996/audio hardware/qcom-caf/msm8996/media
+rm -rf hardware/qcom-caf/msm8996/display hardware/qcom-caf/msm8996/audio hardware/qcom-caf/msm8996/media hardware/qcom-caf/wlan
+git clone https://github.com/Havoc-Devices/android_hardware_qcom-caf_msm8996_audio hardware/qcom-caf/msm8996/audio
+git clone https://github.com/Havoc-Devices/android_hardware_qcom-caf_msm8996_display hardware/qcom-caf/msm8996/display
+git clone https://github.com/Havoc-Devices/android_hardware_qcom-caf_msm8996_media hardware/qcom-caf/msm8996/media
+git clone https://github.com/Havoc-Devices/android_hardware_qcom-caf_wlan hardware/qcom-caf/wlan 
 
 # All cloned stuff should be hosted on havoc-devices , otherwise it won't be cloned
