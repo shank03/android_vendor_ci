c152249a163dc620cec95b696d38a76aeb820977
adnan-44 <mohd.adnan0405@gmail.com>
2021-10-27 23:58:42 +0530

    vince: Clone GCC toolchain also
    * It's required to build DA Kernel
    
    Signed-off-by: adnan-44 <mohd.adnan0405@gmail.com>

--- vince.sh
+++ vince.sh
@@ -10,4 +10,8 @@ git clone https://github.com/Havoc-Devices/android_hardware_qcom-caf_msm8996_dis
 git clone https://github.com/Havoc-Devices/android_hardware_qcom-caf_msm8996_media hardware/qcom-caf/msm8996/media
 git clone https://github.com/Havoc-Devices/android_hardware_qcom-caf_wlan hardware/qcom-caf/wlan 
 
+# GCC toolchain 
+rm -rf prebuilts/gcc/linux-x86/aarch64/aarch64-elf && git clone --depth 1 https://github.com/Havoc-Devices/gcc-arm64.git prebuilts/gcc/linux-x86/aarch64/aarch64-elf
+rm -rf prebuilts/gcc/linux-x86/arm/arm-eabi && git clone --depth 1 https://github.com/Havoc-Devices/gcc-arm.git prebuilts/gcc/linux-x86/arm/arm-eabi
+
 # All cloned stuff should be hosted on havoc-devices , otherwise it won't be cloned
