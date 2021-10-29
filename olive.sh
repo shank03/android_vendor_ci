1d69f5cf02a6b3264fe6d08cc16c722de5aefee4
deadlylxrd <IAmDeadlylxrd@gmail.com>
2021-10-29 19:15:06 +0500

    olive: Track custom hals

--- olive.sh
+++ olive.sh
@@ -2,3 +2,4 @@
 #
 # Track our display hals
 rm -rf hardware/qcom-caf/msm8996/display
+git clone -b lineage-18.1-caf-msm8996 https://github.com/Havoc-Devices/platform_hardware_qcom-caf_msm8996_display hardware/qcom-caf/msm8996/display
