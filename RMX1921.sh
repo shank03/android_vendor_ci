19e38981bdbea1751b06560d1dd94bd8cda75401
improbir <ghosh_probir@yahoo.com>
2021-10-30 06:57:35 +0000

    RMX1921: track havoc remote only
    Signed-off-by: improbir <ghosh_probir@yahoo.com>

--- RMX1921.sh
+++ RMX1921.sh
@@ -1,13 +1,13 @@
 # Track display hal
 rm -rf hardware/qcom-caf/sdm845/display
-
+git clone https://github.com/Havoc-Devices/platform_hardware_qcom-caf_sdm845_display.git -b eleven hardware/qcom-caf/sdm845/display
 
 # Track media hal
 rm -rf hardware/qcom-caf/sdm845/media
-git clone https://github.com/PixelExperience/hardware_qcom-caf_sdm845_media.git -b eleven hardware/qcom-caf/sdm845/media
+git clone https://github.com/Havoc-Devices/platform_hardware_qcom-caf_sdm845_media.git  -b eleven hardware/qcom-caf/sdm845/media
 
 # Cleanup some hals
 rm -rf hardware/qcom-caf/sm8*
 
 # Adapt our device display hal
-cd vendor/havoc && git fetch https://github.com/Havoc-Devices/android_vendor_havoc.git eleven && git cherry-pick 94a7366632d4d57d9183abd4ff2f6d4886eb9d20 && cd ../.. 
+rm -rf  vendor/havoc && git clone https://github.com/Havoc-Devices/android_vendor_havoc.git -b RMX1921 vendor/havoc
