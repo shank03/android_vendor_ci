a153650b397541e95c1efb1bc47cdc8144bbf74a
sukeeratsg <sukeerat@goindi.org>
2021-11-04 19:30:39 +0100

    CI : Run scripts only once

--- instantnoodle.sh
+++ instantnoodle.sh
@@ -1,3 +1,7 @@
-bash device/oneplus/sm8250-common/patch/patch.sh
-git clone -b eleven https://gitlab.com/havoc-devices/android_vendor_oneplus_opcamera vendor/oneplus/opcamera
+if [ ! -f ${rompath}/bash.lock ]; then
+    bash device/oneplus/sm8250-common/patch/patch.sh
+    git clone -b eleven https://gitlab.com/havoc-devices/android_vendor_oneplus_opcamera vendor/oneplus/opcamera
+fi
+
 export buildtype="user"
+echo "Done" > ${rompath}/bash.lock
--- kebab.sh
+++ kebab.sh
@@ -1,3 +1,7 @@
-bash device/oneplus/sm8250-common/patch/patch.sh
-git clone -b eleven https://gitlab.com/havoc-devices/vendor_oneplus_addons_camera.git vendor/oneplus/addons/camera
+if [ ! -f ${rompath}/bash.lock ]; then
+    bash device/oneplus/sm8250-common/patch/patch.sh
+    git clone -b eleven https://gitlab.com/havoc-devices/vendor_oneplus_addons_camera.git vendor/oneplus/addons/camera
+fi
+
 export buildtype="user"
+echo "Done" > ${rompath}/bash.lock
