d95e7b61558b845a618a2c8d423839ce1636e1a4
kevin Frost <frostkevin212@gmail.com>
2021-12-18 07:13:50 -0500

    mido: update hal script and build type

--- mido.sh
+++ mido.sh
@@ -4,4 +4,14 @@
 
 # All cloned stuff should be hosted on havoc-devices , otherwise it won't be cloned
 
-rm -rf hardware/qcom-caf/msm8996/display hardware/qcom-caf/msm8996/audio hardware/qcom-caf/msm8996/media
+rm -rf hardware/qcom-caf/msm8996/display
+git clone https://github.com/Havoc-Devices/android_hardware_qcom_display.git -b eleven hardware/qcom-caf/msm8996/display
+
+rm -rf hardware/qcom-caf/msm8996/meida
+git clone https://github.com/Havoc-Devices/android_hardware_qcom_media.git -b eleven hardware/qcom-caf/msm8996/media
+
+rm -rf hardware/qcom-caf/msm8996/audio
+git clone https://github.com/Havoc-Devices/android_hardware_qcom_audio-1.git -b eleven hardware/qcom-caf/msm8996/audio
+
+# Build type 
+export build_type="user"
