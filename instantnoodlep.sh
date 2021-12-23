c01fd7e6cb3ab9a9a6c948c59e9dfe7bf2795bee
cjh1249131356 <cjh1249131356@gmail.com>
2021-12-23 15:37:03 +0800

    op8/9r: Drop hardware/oneplus

--- instantnoodlep.sh
+++ instantnoodlep.sh
@@ -1,6 +1,7 @@
 if [ ! -f ${rompath}/bash.lock ]; then
     bash device/oneplus/sm8250-common/patch/patch.sh
     git clone -b eleven https://gitlab.com/havoc-devices/android_vendor_oneplus_opcamera vendor/oneplus/opcamera
+    rm -rf hardware/oneplus
 fi
 
 export build_type="user"
--- kebab.sh
+++ kebab.sh
@@ -1,6 +1,7 @@
 if [ ! -f ${rompath}/bash.lock ]; then
     bash device/oneplus/sm8250-common/patch/patch.sh
     git clone -b eleven https://gitlab.com/havoc-devices/vendor_oneplus_addons_camera.git vendor/oneplus/addons/camera
+    rm -rf hardware/oneplus
 fi
 
 export build_type="user"
