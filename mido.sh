9b9984f5bc66250ca9b5367900a72a1f92fcfda2
kevin Frost <frostkevin212@gmail.com>
2021-12-23 02:38:01 -0500

    mido: fix typo mistake

--- mido.sh
+++ mido.sh
@@ -7,7 +7,7 @@
 rm -rf hardware/qcom-caf/msm8996/display
 git clone https://github.com/Havoc-Devices/android_hardware_qcom_display.git -b eleven hardware/qcom-caf/msm8996/display
 
-rm -rf hardware/qcom-caf/msm8996/meida
+rm -rf hardware/qcom-caf/msm8996/media
 git clone https://github.com/Havoc-Devices/android_hardware_qcom_media.git -b eleven hardware/qcom-caf/msm8996/media
 
 rm -rf hardware/qcom-caf/msm8996/audio
