e57af998c4699c2bcd5c27054516db1983f17edd
Anushek Prasal <anushekprasal@gmail.com>
2022-01-01 22:12:12 +0530

    ci: Clone OnePlus apps manually

--- hotdog.sh
+++ hotdog.sh
@@ -1,6 +1,12 @@
 # Havoc-OS
 
-(cd frameworks/native && git fetch havoc && git checkout havoc/eleven-caf)
+if [ ! -f ${rompath}/bash.lock ]; then
+    (cd frameworks/native && git fetch havoc && git checkout havoc/eleven-caf)
+    rm -rf packages/apps/OnePlus
+    git clone -b eleven https://gitlab.com/havoc-devices/android_packages_apps_OnePlus.git packages/apps/OnePlus
+fi
 
 # Build Type
 export build_type="user"
+
+echo "Done" > ${rompath}/bash.lock
