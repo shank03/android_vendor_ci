daffaf1cb2b7723ead7aa7969c97c69d29d7006f
Sukeerat Goindi <sukeerat@goindi.org>
2022-01-02 20:51:21 +0530

    Update mido.sh

--- mido.sh
+++ mido.sh
@@ -1,9 +1,3 @@
-# Havoc-OS
-
-# For Hals
-
-# All cloned stuff should be hosted on havoc-devices , otherwise it won't be cloned
-
 rm -rf hardware/qcom-caf/msm8996/display
 git clone https://github.com/Havoc-Devices/android_hardware_qcom_display.git -b eleven hardware/qcom-caf/msm8996/display
 
