6c49c3288c0d34fa42011411a8f84582621b29de
chandu078 <chandudyavanapelli03@gmail.com>
2022-01-12 18:03:48 +0000

    lemonade/p: Prepare everything to use device_kernel_headers
    Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>

--- lemonadep.sh
+++ lemonadep.sh
@@ -1,6 +1,15 @@
 if [ ! -f ${rompath}/bash.lock ]; then
     bash device/oneplus/sm8350-common/patch/patch.sh
     rm -rf hardware/oneplus
+    rm -rf hardware/qcom-caf/kernel-headers
+    rm -rf hardware/qcom-caf/sm8250/
+    rm -rf hardware/qcom-caf/sm8350/display
+    rm -rf hardware/qcom-caf/sm8350/kernel-headers
+    rm -rf device/qcom/common
+    git clone -b eleven https://github.com/Havoc-Devices/hardware_qcom-caf_sm8350_kernel-headers hardware/qcom-caf/kernel-headers/msm-5.4
+    git clone -b eleven https://github.com/Havoc-Devices/hardware_qcom-caf_sm8350_display hardware/qcom-caf/sm8350/display
+    git clone -b eleven https://github.com/Havoc-Devices/hardware_qcom-caf_sm8350_kernel-headers hardware/qcom-caf/sm8350/kernel-headers
+    git clone -b eleven https://github.com/Havoc-Devices/device_qcom_common device/qcom/common
 fi
 
 export build_type="user"
