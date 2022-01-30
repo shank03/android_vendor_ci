bb94e3802984442029a375e28ae39f8bf6d3f4c5
Joaquin Guzman <githubjoaquin@gmail.com>
2022-01-30 23:25:00 +0800

    mojito: Add patch for my device

--- mojito.sh
+++ mojito.sh
@@ -1,1 +1,9 @@
-export build_type="user"
+if [ ! -f ${source_path}/bash.lock ];
+then
+
+     git -C build/soong apply ${source_path}/device/xiaomi/mojito/patches/soong.patch
+     git -C build/soong add .
+     git -C build/soong commit -m "tmp"
+
+export build_type="userdebug"
+echo "Done" >> ${source_path}/bash.lock
