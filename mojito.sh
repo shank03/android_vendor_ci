6710cbcf2b750ac479b7da3f8c3ec089140c9cc7
Joaquin Guzman <githubjoaquin@gmail.com>
2022-02-02 05:42:10 +0800

    mojito: clone gcc toolchain also

--- mojito.sh
+++ mojito.sh
@@ -1,6 +1,8 @@
 if [ ! -f ${source_path}/bash.lock ];
 then
 
+     git clone -b eleven https://github.com/Havoc-Devices/gcc-arm64 ${source_path}/prebuilts/gcc/linux-x86/aarch64/arm64-gcc
+
      git -C build/soong apply ${source_path}/device/xiaomi/mojito/patches/soong.patch
      git -C build/soong add .
      git -C build/soong commit -m "tmp"
