89d0c667c09443b31c7b66fade316813a2464070
Joaquin Guzman <githubjoaquin@gmail.com>
2022-02-03 22:05:19 +0800

    mojito: Update Gcc

--- mojito.sh
+++ mojito.sh
@@ -1,6 +1,6 @@
 if [ ! -f ${source_path}/bash.lock ];
 then
-     git clone -b eleven https://github.com/Havoc-Devices/gcc-arm64 ${source_path}/prebuilts/gcc/linux-x86/aarch64/arm64-gcc
+     git clone --depth=1 -b eleven https://github.com/Havoc-Devices/android_prebuilts_gcc_linux-x86_aarch64_aarch64-elf ${source_path}/prebuilts/gcc/linux-x86/aarch64/arm64-gcc
      git -C build/soong apply ${source_path}/device/xiaomi/mojito/patches/soong.patch
      git -C build/soong add .
      git -C build/soong commit -m "tmp"
