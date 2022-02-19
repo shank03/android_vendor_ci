fd715f69bfc76e780aeb1a448cf6dc29d66fc6f1
optimus9811 <vikasyadav5073@gmail.com>
2022-02-19 20:04:55 +0530

    UPDATE :Default Clang to clang 14.0.2 r445002
    Signed-off-by: optimus9811 <vikasyadav5073@gmail.com>

--- enchilada.sh
+++ enchilada.sh
@@ -3,6 +3,6 @@ rm -rf hardware/qcom-caf/sdm845/display prebuilts/clang/host/linux-x86/clang-r43
 git clone https://github.com/Havoc-Devices/android_sdm845_qcom_display  -b sdm845   hardware/qcom-caf/sdm845/display  --depth=1
 git clone https://github.com/Havoc-Devices/android_sdm845_qcom_audio   -b sdm845  hardware/qcom-caf/sdm845/audio --depth=1
 git clone https://github.com/Havoc-Devices/android_sdm845_qcom_media  -b sdm845  hardware/qcom-caf/sdm845/media --depth=1
-git clone -b 12.0 https://gitlab.com/havoc-devices/android-prebuilts-clang-host-linux-x86-clang-r437112bb.git  prebuilts/clang/host/linux-x86/clang-r437112b
+git clone -b 12.0 git@gitlab.com:havoc-devices/android_prebuilts_clang_host_linux-x86_clang-r445002.git  prebuilts/clang/host/linux-x86/clang-r445002
 
 export build_type="user"
