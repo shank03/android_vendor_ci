61444712164e32de71d6e41ab8bf09ea0d02238c
Sukeerat Goindi <sukeerat@goindi.org>
2022-02-24 09:03:36 +0530

    X01BD : Build neverallows

--- X01BD.sh
+++ X01BD.sh
@@ -1,2 +1,3 @@
 rm -rf hardware/qcom-caf/msm8998/display
 git clone https://github.com/Havoc-Devices/hardware_qcom-caf_msm8998_display  -b eleven hardware/qcom-caf/msm8998/display
+export SELINUX_IGNORE_NEVERALLOWS=true
