680397c5af462ab205035009e2bcf002de1e7516
deadlylxrd <contact@deadlylxrd.me>
2022-03-28 17:04:56 +0500

    mi439: Welcome

---
+++ mi439.sh
@@ -0,0 +1,5 @@
+# Havoc-OS
+#
+# Track patched audio hal
+rm -rf hardware/qcom-caf/msm8996/audio
+git clone -b lineage-18.1-caf-msm8996 https://github.com/IAmDeadlylxrd/android_hardware_qcom_audio hardware/qcom-caf/msm8996/audio
