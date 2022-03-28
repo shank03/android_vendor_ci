ce94d54e49dd2838e4b60170c9da5ae266ad4fb7
deadlylxrd <contact@deadlylxrd.me>
2022-03-28 17:13:41 +0500

    mi439: Track audio hal from Havoc-Devices

--- mi439.sh
+++ mi439.sh
@@ -2,4 +2,4 @@
 #
 # Track patched audio hal
 rm -rf hardware/qcom-caf/msm8996/audio
-git clone -b lineage-18.1-caf-msm8996 https://github.com/IAmDeadlylxrd/android_hardware_qcom_audio hardware/qcom-caf/msm8996/audio
+git clone -b lineage-18.1-caf-msm8996 https://github.com/Havoc-Devices/android_hardware_qcom_audio_mi439 hardware/qcom-caf/msm8996/audio
