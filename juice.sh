ebcee18d71d0562d22f0070fb6dea20aac34f73f
Sukeerat Goindi <sukeerat@goindi.org>
2022-04-03 08:20:12 +0530

    Juice : Clone manually cause roomservice defaults to python2 and cannot parse UTF8 encoding by Github API right now
    Signed-off-by: sukeeratsg <sukeerat@goindi.org>

--- juice.sh
+++ juice.sh
@@ -1,2 +1,5 @@
 export build_type="user"
 git clone --depth=1 https://gitlab.com/havoc-devices/android-prebuilts-clang-host-linux-x86-clang-r437112bb prebuilts/clang/host/linux-x86/clang-r437112b
+git clone git@github.com:Havoc-Devices/android_vendor_xiaomi_juice.git vendor/xiaomi/juice --depth=1
+git clone git@github.com:Havoc-Devices/android_kernel_xiaomi_juice.git kernel/xiaomi/juice --depth=1
+git clone git@github.com:Havoc-Devices/android_device_xiaomi_juice.git device/xiaomi/juice --depth=1
