68bd47a535f588177b42752915582d81d74c5f1d
Vitorgl2003 <vitorgl.2003@gmail.com>
2022-04-22 11:29:56 +0100

    lavender: Drop uneened dependencies

--- lavender.sh
+++ lavender.sh
@@ -1,9 +1,8 @@
-rm -rf hardware/qcom-caf/msm8998/media vendor/havoc/charger hardware/qcom-caf/msm8998/display  hardware/qcom-caf/msm8998/audio   packages/resources/devicesettings  device/xiaomi/extras
+rm -rf hardware/qcom-caf/msm8998/media vendor/havoc/charger hardware/qcom-caf/msm8998/display hardware/qcom-caf/msm8998/audio packages/resources/devicesettings
 
 git clone https://github.com/ArrowOS/android_hardware_qcom_media -b arrow-11.0-caf-msm8998 hardware/qcom-caf/msm8998/media
 git clone https://github.com/ArrowOS/android_hardware_qcom_audio -b arrow-11.0-caf-msm8998 hardware/qcom-caf/msm8998/audio
 git clone https://github.com/ArrowOS/android_hardware_qcom_display -b arrow-11.0-caf-msm8998 hardware/qcom-caf/msm8998/display
 git clone https://github.com/Vitorgl2003/packages_resources_devicesettings packages/resources/devicesettings
-git clone https://github.com/Vitorgl2003/device_xiaomi_extras device/xiaomi/extras
 
 export build_type='user'
