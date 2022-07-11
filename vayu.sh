22fdd45f172e3b34b8d38aea5f2f2d5344acf629
Alisson Grizotti <alisson1397@hotmail.com>
2022-07-11 17:09:57 -0300

    vayu: Added our dependencies

---
+++ vayu.sh
@@ -0,0 +1,5 @@
+git clone https://github.com/Havoc-Devices/android_device_xiaomi_vayu device/xiaomi/vayu 
+git clone https://github.com/Havoc-Devices/android_vendor_xiaomi_vayu vendor/xiaomi/vayu
+git clone https://github.com/Havoc-Devices/android_kernel_xiaomi_vayu kernel/xiaomi/vayu
+git clone https://github.com/Havoc-Devices/android_hardware_xiaomi hardware/xiaomi
+git clone https://github.com/Havoc-Devices/android_packages_resources_devicesettings packages/resources/devicesettings
