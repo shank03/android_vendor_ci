3423c8483182cc8e19a6ca2b43592bb3f1dbd614
Shashank Verma <39261691+shank03@users.noreply.github.com>
2022-07-16 12:16:02 +0530

    lilac: Add dependencies

---
+++ lilac.sh
@@ -0,0 +1,11 @@
+git clone https://github.com/Havoc-Devices/android_device_sony_lilac.git -b twelve device/sony/lilac
+git clone https://github.com/Havoc-Devices/android_device_sony_yoshino-common.git -b twelve device/sony/yoshino-common
+git clone https://github.com/Havoc-Devices/android_kernel_sony_msm8998.git -b twelve kernel/sony/msm8998
+git clone https://github.com/Havoc-Devices/android_vendor_sony_lilac.git -b twelve vendor/sony/lilac
+git clone https://github.com/Havoc-Devices/android_packages_resources_devicesettings packages/resources/devicesettings
+
+export HAVOC_MAINTAINER="Shashank Verma (shank03)"
+export HAVOC_GROUP_URL=https://t.me/havoc_lilac
+export HAVOC_BUILD_TYPE=Official
+export WITHOUT_CHECK_API=true
+export BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES=true
